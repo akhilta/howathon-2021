41a854e768112a4668cd744565cdb82f540a1c2c
tamreddy1 <tamatam.reddy@publicissapient.com>
2021-02-28 20:18:52 +0530

    some changes

--- src/main/java/com/sapient/teamsApi/Controller/CitationDataController.java
+++ src/main/java/com/sapient/teamsApi/Controller/CitationDataController.java
@@ -7,11 +7,11 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sapient.teamsApi.Data.CitationTableRepo;
@@ -23,6 +23,7 @@ import com.sapient.teamsApi.Helpers.JsonToStringToJson;
 import com.sapient.teamsApi.Service.TeamsApiServiceImplementation;
 
 @RestController
+@CrossOrigin
 public class CitationDataController {
 
 	@Autowired
